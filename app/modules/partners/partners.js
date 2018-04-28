b414f15683e503fd7ddf180d9cdb23e08c9e284a
eblondel <emmanuel.blondel1@gmail.com>
2018-04-28 22:50:55 +0200

    update partners page

--- app/modules/partners/partners.js
+++ app/modules/partners/partners.js
@@ -7,22 +7,20 @@
 angular.module('partners').controller('PartnersCtrl',
 		[ '$scope', '$translate', '$timeout',
           function($scope, $translate, $timeout) {
-
-			//clients
-			$scope.clients = [];
-			$timeout(function(){
-				$scope.clients =[
-					{id: 'fao', name: 'UN-FAO', url: 'http://www.fao.org'},
-					{id: 'ifremer', name: 'Ifremer', url: 'http://wwz.ifremer.fr/'},
-					{id: 'umanis', name: 'Umanis', url: 'http://www.umanis.com'}
-				];
-			});
 			
 			//collaborators
 			$scope.collaborators = [];
 			$timeout(function(){
-				$scope.collaborators =[
-					{id: 'grida', name: 'GRID-ARENDAL', url: 'https://www.grida.no/'}
+				$scope.collaborators = [
+					{id: 'fao', name: 'UN-FAO', url: 'http://www.fao.org'},
+					{id: 'ifremer', name: 'Ifremer', url: 'http://wwz.ifremer.fr/'},
+					{id: 'grida', name: 'GRID-ARENDAL', url: 'https://www.grida.no/'},
+					{id: 'ird', name: 'IRD', url: 'https://www.ird.fr/'},
+					{id: 'ird-marbec', name: 'IRD MARBEC Unit', url: 'http://www.umr-marbec.fr/fr/'},
+					{id: 'cnr-isti', name: 'CNR ISTI', url: 'http://www.isti.cnr.it/'},
+					{id: 'd4science', name: 'D4Science', url: 'https://www.d4science.org/'},
+					{id: 'imarine', name: 'i-Marine', url: 'http://www.i-marine.eu/Pages/Home.aspx'},
+					{id: 'bluebridge', name: 'BlueBridge', url: 'http://www.bluebridge-vres.eu/'}		
 				];
 			});
 
